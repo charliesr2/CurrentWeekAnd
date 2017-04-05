b66e241d5be838426e4b4a5f32ab6d0efe8943ec
Carlos <charliesr2@gmail.com>
2017-04-06 01:39:14 +0200

    Finiquitando app

--- app/src/main/java/edu/uoc/android/currentweek/pantalla1.java
+++ app/src/main/java/edu/uoc/android/currentweek/pantalla1.java
@@ -1,14 +1,64 @@
 package edu.uoc.android.currentweek;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class pantalla1 extends AppCompatActivity {
+public class pantalla1 extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText mEditTextWeekNumber;
+    private Button mButtonCheck;
+    private String sKeyCurrentWeek = "currentWeekUser";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla1);
+
+        // arreguem vistes
+        mEditTextWeekNumber = (EditText) findViewById(R.id.et1);
+        mButtonCheck = (Button) findViewById(R.id.btCheck);
+
+        // apliquem listeners pels events
+
+        mButtonCheck.setOnClickListener(this);
+
+        if (savedInstanceState != null)
+        {
+            mEditTextWeekNumber.setText(savedInstanceState.getString(sKeyCurrentWeek));
+        }
+    }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState)
+    {
+        savedInstanceState.putString(sKeyCurrentWeek,mEditTextWeekNumber.getText().toString());
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    public void onClick(View v)
+    {
+
+        if (v == mButtonCheck)
+        {
+            if (mEditTextWeekNumber.getText().toString().isEmpty())
+            {
+                mEditTextWeekNumber.setError("Introduce the current week number");
+                return;
+            }
+            startActivityPantalla2();
+        }
+    }
+
+    private void startActivityPantalla2() {
+        Intent intent = new Intent(this,pantalla2.class);
+        intent.putExtra(sKeyCurrentWeek,mEditTextWeekNumber.getText());
+        startActivity(intent);
+
     }
 }
--- app/src/main/java/edu/uoc/android/currentweek/pantalla2.java
+++ app/src/main/java/edu/uoc/android/currentweek/pantalla2.java
@@ -1,13 +1,46 @@
 package edu.uoc.android.currentweek;
 
+import java.util.Calendar;
+
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class pantalla2 extends AppCompatActivity {
+
+public class pantalla2 extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mTextViewResultado;
+    private Button mButtonStartOver;
+    private Calendar calendario = Calendar.getInstance();
+    private String sKeyCurrentWeek = "currentWeekUser";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla2);
+        mTextViewResultado = (TextView) findViewById(R.id.tv1);
+        mButtonStartOver = (Button) findViewById(R.id.bt1);
+        mButtonStartOver.setOnClickListener(this);
+        int diaSemana = calendario.get(Calendar.DAY_OF_WEEK);
+
+        if (getIntent().getExtras().getInt(sKeyCurrentWeek) == diaSemana) {
+            mButtonStartOver.setText("START AGAIN");
+            mTextViewResultado.setTextColor(Color.GREEN);
+            mTextViewResultado.setText("Right!!");
+        } else {
+            mButtonStartOver.setText("START AGAIN");
+            mTextViewResultado.setTextColor(Color.RED);
+            mTextViewResultado.setText("You have failed, try again!!");
+        }
     }
+    public void onClick(View v)
+    {
+        this.finish();
+    }
+
+
 }
